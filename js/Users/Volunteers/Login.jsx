a90e2ade317995bbddc3d8b1917d0d924d73f865
Jwing28 <jonathan.wing.lee@gmail.com>
2017-10-03 14:13:17 -0700

    created styling for forms

--- js/Users/Volunteers/Login.jsx
+++ js/Users/Volunteers/Login.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { existingUser } from '../../App/actions'; //since state and db are synced don't need this atm.
-
+import { Table, Button } from 'react-bootstrap';
 
 class Login extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { name:'', email:'', valid:false }
+    this.state = { name: '', email: '', valid: false };
 
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
@@ -24,8 +24,8 @@ class Login extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     var UserData = {
-      name:this.state.name,
-      email:this.state.email
+      name: this.state.name,
+      email: this.state.email
     };
 
     var userExist = this.props.volunteers.filter(function(volunteerObj) {
@@ -33,39 +33,83 @@ class Login extends React.Component {
       return email === UserData.email;
     });
 
-    if(userExist.length) {
+    if (userExist.length) {
       this.setState({ valid: !this.state.valid });
       //check if someone is already logged in
-      localStorage.setItem('email',UserData.email); //no one is logged in, ok to login
+      localStorage.setItem('email', UserData.email); //no one is logged in, ok to login
     }
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div><label>Name: <input type="text" name="Name" value={this.state.name} onChange={this.handleNameChange} required /></label></div>
-        <div><label>Email: <input type="text" name="Email"  value={this.state.email} onChange={this.handleEmailChange} required /></label></div>
-        <input type="submit" value="Login" />
+      <form style={formStyles.form} onSubmit={this.handleSubmit}>
+        <label style={formStyles.contents}>
+          Name:{' '}
+          <input
+            style={formStyles.contents}
+            type="text"
+            name="Name"
+            value={this.state.name}
+            onChange={this.handleNameChange}
+            required
+          />
+        </label>
+        <label style={formStyles.contents}>
+          Email:{' '}
+          <input
+            style={formStyles.contents}
+            type="text"
+            name="Email"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+            required
+          />
+        </label>
+        <input style={formStyles.button} type="submit" value="Login" />
         <CheckUser userInfo={this.state} />
       </form>
-    )
+    );
   }
 }
 
-const CheckUser = (props) => {
+//put this in css file
+const formStyles = {
+  form: {
+    border: '2px solid black',
+    borderImage: 'linear-gradient(to right, red, blue) 1',
+    padding: '10px',
+    width: '350px',
+    borderRadius: '5px',
+    textAlign: 'left',
+    margin: '5% auto'
+  },
+  contents: {
+    display: 'block',
+    margin: '10px 0px'
+  },
+  button: {
+    backgroundColor: 'lightblue',
+    border: '1px solid black',
+    borderRadius: '5px',
+    fontSize: '15px',
+    marginBottom: '10px'
+  }
+};
+
+const CheckUser = props => {
   const validUser = props.userInfo.valid;
 
-  if(validUser) {
-    return <h3>Welcome! {props.userInfo.name} </h3>;
-  }else {
-    return <h3>User was not found. Try again or register as new user.</h3>
+  if (validUser) {
+    return <div>Welcome! {props.userInfo.name} </div>;
+  } else {
+    return <div>User was not found. Try again or register as new user.</div>;
   }
-}
+};
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     volunteers: state.volunteers
-  }
+  };
 };
 
 export default connect(mapStateToProps)(Login);
--- js/Users/Volunteers/Register.jsx
+++ js/Users/Volunteers/Register.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Table, Button } from 'react-bootstrap';
 import { createNewVolunteer } from '../../App/actions';
 import VolunteerSummary from './VolunteerSummary';
 
 class Register extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { name:'', email:'', age:0, valid:false }
+    this.state = { name: '', email: '', age: 0, valid: false };
 
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
@@ -17,7 +18,7 @@ class Register extends React.Component {
   handleNameChange(e) {
     this.setState({ name: e.target.value });
     //resets message since user is trying again or trying for first time to register
-    if(this.state.valid){
+    if (this.state.valid) {
       this.setState({ valid: !this.state.valid });
     }
   }
@@ -33,11 +34,11 @@ class Register extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     var UserData = {
-      name:this.state.name,
-      email:this.state.email,
-      age:this.state.age,
+      name: this.state.name,
+      email: this.state.email,
+      age: this.state.age,
       eventsJoined: 0,
-      futureEvents: []      
+      futureEvents: []
     };
 
     var userExist = this.props.volunteers.filter(function(volunteerObj) {
@@ -45,44 +46,102 @@ class Register extends React.Component {
       return email === UserData.email;
     });
 
-    if(userExist.length) {
-      console.log('user does exist')
+    if (userExist.length) {
+      console.log('user does exist');
       this.setState({ valid: !this.state.valid });
-    }else { //user does not exist - save user to database
+    } else {
+      //user does not exist - save user to database
       this.props.createNewVolunteer(UserData);
     }
   }
 
   render() {
     return (
-        <form onSubmit={this.handleSubmit}>
-          <div><label>Name: <input type="text" name="Name" value={this.state.name} onChange={this.handleNameChange} required /></label></div>
-          <div><label>Email: <input type="text" name="Email"  value={this.state.email} onChange={this.handleEmailChange} required /></label></div>
-          <div><label>Age: <input type="text" name="Age" value={this.state.age} onChange={this.handleAgeChange} required /></label></div>
-          <input type="submit" value="Register" />
-          <CheckUser userInfo={this.state} />
-        </form>
-    )
+      <form style={formStyles.form} onSubmit={this.handleSubmit}>
+        <label style={formStyles.contents}>
+          Name:{' '}
+          <input
+            style={formStyles.contents}
+            type="text"
+            name="Name"
+            value={this.state.name}
+            onChange={this.handleNameChange}
+            required
+          />
+        </label>
+        <label style={formStyles.contents}>
+          Email:{' '}
+          <input
+            style={formStyles.contents}
+            type="text"
+            name="Email"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+            required
+          />
+        </label>
+        <label style={formStyles.contents}>
+          Age:{' '}
+          <input
+            style={formStyles.contents}
+            type="text"
+            name="Age"
+            value={this.state.age}
+            onChange={this.handleAgeChange}
+            required
+          />
+        </label>
+        <input style={formStyles.button} type="submit" value="Register" />
+        <CheckUser userInfo={this.state} />
+      </form>
+    );
   }
 }
-//need to have case to notify user they have successfully registered
-const CheckUser = (props) => {
-  const validUser = props.userInfo.valid;
 
-  if(!validUser) {
-    return <h3>Welcome! {props.userInfo.name} Please sign up for an event(s). </h3>;
-  }else {
-    return <h3>Sorry, {props.userInfo.name}, user already exists. Please login as existing user or retry registering.</h3>
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    volunteers: state.volunteers
+//put this in css file
+const formStyles = {
+  form: {
+    border: '2px solid black',
+    borderImage: 'linear-gradient(to right, red, blue) 1',
+    padding: '10px',
+    width: '350px',
+    borderRadius: '5px',
+    textAlign: 'left',
+    margin: '5% auto'
+  },
+  contents: {
+    display: 'block',
+    margin: '10px 0px'
+  },
+  button: {
+    backgroundColor: 'lightblue',
+    border: '1px solid black',
+    borderRadius: '5px',
+    fontSize: '15px',
+    marginBottom: '10px'
   }
 };
 
-const mapDispatchToProps = (dispatch) => ({
+//need to have case to notify user they have successfully registered
+const CheckUser = props => {
+  const validUser = props.userInfo.valid;
+
+  if (!validUser) {
+    return <div>Welcome! {props.userInfo.name} Please sign up for an event(s). </div>;
+  } else {
+    return (
+      <div>Sorry, {props.userInfo.name}, user already exists. Please login as existing user or retry registering.</div>
+    );
+  }
+};
+
+const mapStateToProps = state => {
+  return {
+    volunteers: state.volunteers
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
   createNewVolunteer(UserInfo) {
     dispatch(createNewVolunteer(UserInfo));
   }
